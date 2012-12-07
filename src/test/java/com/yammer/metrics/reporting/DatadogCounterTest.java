f03244273a95e1dc8272bddc3a270d9fb7b2b80e
Joe DeVries <joe.devries@bazaarvoice.com>
2012-12-07 17:14:48 -0600

    Revert "Allow some special characters in metric names and tags"
    This reverts commit fc0e88fdaf04ac65909d35ae0b05db79a2a7d098.

--- src/test/java/com/yammer/metrics/reporting/DatadogCounterTest.java
+++ src/test/java/com/yammer/metrics/reporting/DatadogCounterTest.java
@@ -1,11 +1,12 @@
 package com.yammer.metrics.reporting;
 
-import com.yammer.metrics.reporting.model.DatadogCounter;
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import com.yammer.metrics.reporting.model.DatadogCounter;
 
 public class DatadogCounterTest {
 
@@ -22,16 +23,15 @@ public class DatadogCounterTest {
     }
 
     @Test
-    public void testStripInvalidCharsFromTagsAndNames() {
+    public void testStripInvalidCharsFromTags() {
         DatadogCounter counter = new DatadogCounter(
-                "test.name,with_chars[tag.1:va  lue.1,tag-2:va %lue-2,ta  %# g_3:value_3]", 1L, 1234L, "Test Host");
+                "test[tag1:va  lue1,tag2:va .%lue2,ta  %# g3:value3]", 1L, 1234L, "Test Host");
         List<String> tags = counter.getTags();
 
         assertEquals(3, tags.size());
-        assertEquals("tag.1:value.1", tags.get(0));
-        assertEquals("tag-2:value-2", tags.get(1));
-        assertEquals("tag_3:value_3", tags.get(2));
-
-        assertEquals("test.name_with_chars", counter.getMetric());
+        assertEquals("tag1:value1", tags.get(0));
+        assertEquals("tag2:value2", tags.get(1));
+        assertEquals("tag3:value3", tags.get(2));
     }
+
 }
